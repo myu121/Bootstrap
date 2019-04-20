200f0a15f9fc8d05504ba0af0f4a381990373f13
Steven Xu <stevenxu1995@gmail.com>
2019-04-20 16:31:32 -0400

    Update paired_bootstrap.R

--- paired_bootstrap.R
+++ paired_bootstrap.R
@@ -1,12 +1,13 @@
 library(boot)
 library(quantreg)
 set.seed(793)
-MC_num = 1000
+MC_num = 100
 n = 100
 boot_num = 1000
 beta <- c(1,1,1)
 qreg_fun <- function(df,ind){
-  fit <- summary(rq(y~x1+x2,tau=0.5,data=df[ind,]),se="nid")
+  exp_weight <- rexp(n)
+  fit <- summary(rq(y~x1+x2,tau=0.5,data=df[ind,],weights = exp_weight),se="nid")
   boot_coef = as.numeric(fit$coefficients[,1])
   boot_se = as.numeric(fit$coefficients[,2])
   return(c(boot_coef,boot_se))
@@ -14,6 +15,9 @@ qreg_fun <- function(df,ind){
 MC.ci.b0 <- matrix(0,ncol=2,nrow=MC_num)
 MC.ci.b1 <- matrix(0,ncol=2,nrow=MC_num)
 MC.ci.b2 <- matrix(0,ncol=2,nrow=MC_num)
+MC.per.b0 <- matrix(0,ncol=2,nrow=MC_num)
+MC.per.b1 <- matrix(0,ncol=2,nrow=MC_num)
+MC.per.b2 <- matrix(0,ncol=2,nrow=MC_num)
 tic("Paired Bootstrap")
 for(i in 1:MC_num){
   x1 <- rlnorm(n)
@@ -26,12 +30,20 @@ for(i in 1:MC_num){
   options(warn=-1)
   boot.out <- boot(data=boot_df,qreg_fun,R=999)
   options(warn=0)
+  MC.per.b0[i,] <- as.numeric(quantile(boot.out$t[1,],c(0.05,0.95)))
+  MC.per.b1[i,] <- as.numeric(quantile(boot.out$t[2,],c(0.05,0.95)))
+  MC.per.b2[i,] <- as.numeric(quantile(boot.out$t[3,],c(0.05,0.95)))
   MC.ci.b0[i,] <- boot.ci(boot.out,type="stud",index=c(1,4))$student[4:5]
   MC.ci.b1[i,] <- boot.ci(boot.out,type="stud",index=c(2,5))$student[4:5]
   MC.ci.b2[i,] <- boot.ci(boot.out,type="stud",index=c(3,6))$student[4:5]
 }
 toc()
 
+#Bootstrap-t CI
 length(which((MC.ci.b0[,1]<beta[1])&(MC.ci.b0[,2]>beta[1])))/MC_num
 length(which((MC.ci.b1[,1]<beta[2])&(MC.ci.b1[,2]>beta[2])))/MC_num
 length(which((MC.ci.b2[,1]<beta[3])&(MC.ci.b2[,2]>beta[3])))/MC_num
+#Naive-percentile CI
+length(which((MC.per.b0[,1]<beta[1])&(MC.per.b0[,2]>beta[1])))/MC_num
+length(which((MC.per.b1[,1]<beta[2])&(MC.per.b1[,2]>beta[2])))/MC_num
+length(which((MC.per.b2[,1]<beta[3])&(MC.per.b2[,2]>beta[3])))/MC_num
