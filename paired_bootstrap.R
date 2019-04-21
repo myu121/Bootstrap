eb8f0858beb5b7d6788289b2971700237865285e
Steven Xu <stevenxu1995@gmail.com>
2019-04-21 00:46:07 -0400

    Update paired_bootstrap.R

--- paired_bootstrap.R
+++ paired_bootstrap.R
@@ -1,7 +1,7 @@
 library(boot)
 library(quantreg)
 set.seed(793)
-MC_num = 10000
+MC_num = 10
 n = 100
 boot_num = 1000
 beta <- c(1,1,1)
@@ -20,7 +20,6 @@ MC.ci.b2 <- matrix(0,ncol=2,nrow=MC_num)
 MC.per.b0 <- matrix(0,ncol=2,nrow=MC_num)
 MC.per.b1 <- matrix(0,ncol=2,nrow=MC_num)
 MC.per.b2 <- matrix(0,ncol=2,nrow=MC_num)
-tic("Paired Bootstrap")
 for(i in 1:MC_num){
   x1 <- rlnorm(n)
   x2 <- c(rep(1,n*0.8),rep(0,n*0.2))
@@ -35,17 +34,22 @@ for(i in 1:MC_num){
   MC.per.b0[i,] <- as.numeric(quantile(boot.out$t[,1],c(0.05,0.95)))
   MC.per.b1[i,] <- as.numeric(quantile(boot.out$t[,2],c(0.05,0.95)))
   MC.per.b2[i,] <- as.numeric(quantile(boot.out$t[,3],c(0.05,0.95)))
-  MC.ci.b0[i,] <- boot.ci(boot.out,type="stud",index=c(1,4))$student[4:5]
-  MC.ci.b1[i,] <- boot.ci(boot.out,type="stud",index=c(2,5))$student[4:5]
-  MC.ci.b2[i,] <- boot.ci(boot.out,type="stud",index=c(3,6))$student[4:5]
+  MC.ci.b0[i,] <- boot.ci(boot.out,type="stud",index=c(1,4),conf = 0.9)$student[4:5]
+  MC.ci.b1[i,] <- boot.ci(boot.out,type="stud",index=c(2,5),conf = 0.9)$student[4:5]
+  MC.ci.b2[i,] <- boot.ci(boot.out,type="stud",index=c(3,6),conf = 0.9)$student[4:5]
 }
-toc()
 
-#Bootstrap-t CI
-length(which((MC.ci.b0[,1]<beta[1])&(MC.ci.b0[,2]>beta[1])))/MC_num
-length(which((MC.ci.b1[,1]<beta[2])&(MC.ci.b1[,2]>beta[2])))/MC_num
-length(which((MC.ci.b2[,1]<beta[3])&(MC.ci.b2[,2]>beta[3])))/MC_num
-#Naive-percentile CI
-length(which((MC.per.b0[,1]<beta[1])&(MC.per.b0[,2]>beta[1])))/MC_num
-length(which((MC.per.b1[,1]<beta[2])&(MC.per.b1[,2]>beta[2])))/MC_num
-length(which((MC.per.b2[,1]<beta[3])&(MC.per.b2[,2]>beta[3])))/MC_num
+#Coverage
+coverage.out = c(length(which((MC.ci.b0[,1]<beta[1])&(MC.ci.b0[,2]>beta[1]))),length(which((MC.ci.b1[,1]<beta[2])&(MC.ci.b1[,2]>beta[2]))),
+                 length(which((MC.ci.b2[,1]<beta[3])&(MC.ci.b2[,2]>beta[3]))),length(which((MC.per.b0[,1]<beta[1])&(MC.per.b0[,2]>beta[1]))),
+                 length(which((MC.per.b1[,1]<beta[2])&(MC.per.b1[,2]>beta[2]))),length(which((MC.per.b2[,1]<beta[3])&(MC.per.b2[,2]>beta[3]))))/MC_num
+#Mean length
+ave.length = c(mean(MC.per.b0[,2]- MC.per.b0[,1]),mean(MC.per.b1[,2]- MC.per.b1[,1]),mean(MC.per.b2[,2]- MC.per.b2[,1]), 
+               mean(MC.ci.b0[,2]- MC.ci.b0[,1]),mean(MC.ci.b1[,2]- MC.ci.b1[,1]),mean(MC.ci.b2[,2]- MC.ci.b2[,1]))
+#SD length
+sd.length = c(sd(MC.per.b0[,2]- MC.per.b0[,1]),sd(MC.per.b1[,2]- MC.per.b1[,1]),sd(MC.per.b2[,2]- MC.per.b2[,1]), 
+              sd(MC.ci.b0[,2]- MC.ci.b0[,1]),sd(MC.ci.b1[,2]- MC.ci.b1[,1]),sd(MC.ci.b2[,2]- MC.ci.b2[,1]))
+summary.out = rbind(coverage.out,ave.length,sd.length)
+colnames(summary.out)=c("NP.b0","NP.b1","NP.b2","BT.b0","BT.b1","BT.b2")
+rownames(summary.out)=c("Coverage","Mean","SD")
+summary.out
